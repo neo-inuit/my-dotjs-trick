51931f05ddc9e171396937df2ec7c3a03e30f13e
Remi Michelas <remi.michelas@inria.fr>
2014-06-18 16:22:59 +0200

    google-script

---
+++ google.fr.js
@@ -0,0 +1,6 @@
+// swap google logo with trollface, octodex images
+$('#hplogo').css({
+  //background: 'url(//bit.ly/ghD24e) no-repeat',
+  background: 'url(http://octodex.github.com/images/dojocat.jpg) no-repeat',
+  backgroundSize: 'auto 95px'
+});
