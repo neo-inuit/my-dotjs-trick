9d02f327abdcf45a1914e6f717059d5b01861aec
remi nitrous <remi@nitrous.io>
2014-06-19 13:43:42 +0000

    try ninja dashboard less size

--- default.js
+++ default.js
@@ -2,5 +2,5 @@
 $('.jumbotron').css({
   //background: 'url(//bit.ly/ghD24e) no-repeat',
   background: 'url(http://octodex.github.com/images/dojocat.jpg) no-repeat',
-  backgroundSize: 'auto 95px'
+  backgroundSize: 'auto 35px'
 });
