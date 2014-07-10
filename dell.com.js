0d314f675a0e08ca6ed95d7b02e27add7fcc3d1e
Remi Michelas <remi.michelas@inria.fr>
2014-07-10 18:56:27 +0200

    example to fill a form

---
+++ dell.com.js
@@ -0,0 +1,8 @@
+// fill the contact mail form
+//
+$('#CompanyName').val("");
+$('#StreetAddress').val("");
+$('#StreetAddress1').val("");
+$('#City').val("");
+$('#Zip').val("");
+$('#PrimaryPhoneNumber').val("");
