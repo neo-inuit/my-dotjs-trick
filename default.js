5b3bbdb9102b86d80c582b2c1c2332f4058214c5
neo <r.michelas@gmail.com>
2014-10-13 15:42:10 +0200

    Update default.js

--- default.js
+++ default.js
@@ -8,7 +8,7 @@ $('.jumbotron').css({
 
 $('head').prepend('<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet"><script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script><script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>');
 
-$('body').prepend('<div class="span2"><ul class="nav nav-pills nav-stacked nav-stacked-menu"><li class=""><a href="/kali/kali/edit" class="stat-tab tab "><i class="icon-edit"></i>Edit Project</a></li><li class=""><a href="/kali/kali/team" class="team-tab tab"><i class="icon-group"></i>Members</a></li><li class=""><a href="/kali/kali/deploy_keys"><span></span>Deploy Keys</a></li><li class="active"><a href="/kali/kali/hooks"><span></span>Web Hooks</a></li><li class=""><a href="/kali/kali/services"><span></span>Services</a></li><li><a href="#" class="btn btn-mini center" onclick="checkAll()"><i class="icon-info"> Check all </i></a></li></ul></div>');
+$('body').prepend('<div class="span2 float"><ul class="nav nav-pills nav-stacked nav-stacked-menu"><li class=""><a href="/kali/kali/edit" class="stat-tab tab "><i class="icon-edit"></i>Edit Project</a></li><li class=""><a href="/kali/kali/team" class="team-tab tab"><i class="icon-group"></i>Members</a></li><li class=""><a href="/kali/kali/deploy_keys"><span></span>Deploy Keys</a></li><li class="active"><a href="/kali/kali/hooks"><span></span>Web Hooks</a></li><li class=""><a href="/kali/kali/services"><span></span>Services</a></li><li><button class="btn btn-mini center" onclick="checkAll()">Check all </button></li></ul></div>');
 
 
 $('.span2').css({background: 'url(https://octodex.github.com/images/dojocat.jpg) no-repeat'});
