63c5dbdf9256bf1720b82e289832c321b9d5227f
neo <r.michelas@gmail.com>
2014-10-13 15:55:16 +0200

    Update default.js

--- default.js
+++ default.js
@@ -6,6 +6,13 @@ $('.jumbotron').css({
   backgroundSize: 'auto 40px'
 });
 
+// a script to check all boxes in a page checkAll()
+
+function checkAll()
+{
+     cbs = document.getElementsByTagName('input');for(var i=0; i < cbs.length; i++){if(cbs[i].type == 'checkbox'){cbs[i].checked = true;}};
+}
+
 //$('head').prepend('<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet"><script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script><script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>');
 
 //$('body').prepend('<div class="span2 float"><ul class="nav nav-pills nav-stacked nav-stacked-menu"><li class=""><a href="/kali/kali/edit" class="stat-tab tab "><i class="icon-edit"></i>Edit Project</a></li><li class=""><a href="/kali/kali/team" class="team-tab tab"><i class="icon-group"></i>Members</a></li><li class=""><a href="/kali/kali/deploy_keys"><span></span>Deploy Keys</a></li><li class="active"><a href="/kali/kali/hooks"><span></span>Web Hooks</a></li><li class=""><a href="/kali/kali/services"><span></span>Services</a></li><li><button class="btn btn-mini center" onclick="checkAll()">Check all </button></li></ul></div>');
@@ -16,11 +23,3 @@ $('.jumbotron').css({
 //$('.nav').css({"list-style": "none", "float": "right"});
 
 $('body').append('<button onclick="CheckAll()">Check All</button>');
-
-// a script to check all boxes in a page checkAll()
-
-
-function checkAll()
-{
-     cbs = document.getElementsByTagName('input');for(var i=0; i < cbs.length; i++){if(cbs[i].type == 'checkbox'){cbs[i].checked = true;}};
-}
